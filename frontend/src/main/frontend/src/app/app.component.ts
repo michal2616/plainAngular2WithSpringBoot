5204db8c461bbfa1cab2629bd37a7c3f2becf16c
michal.gwiazda <michal.gwiazda@yahoo.com>
2017-05-07 00:20:03 +0200

    Plain angular2 + springBoot project

---
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+//   template: `
+// <p>Hi there</p>
+// <ul>
+// <li>Hi there list</li>
+// </ul>`,
+//   styles: ['p{ font-weight: bold;} ul li {text-decoration:underline}']
+})
+export class AppComponent {
+  title = 'app workaaaas!';
+}
