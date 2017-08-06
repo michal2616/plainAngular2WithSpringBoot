22ab842d2729f9815b9a78de8a5d4ab6a8aa6838
michal.gwiazda <michal.gwiazda@yahoo.com>
2017-08-06 13:58:15 +0200

    finished 4th stage Multiuple Components

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
