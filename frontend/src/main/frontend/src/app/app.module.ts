249480f427b5722a413c1124d19cfd3b2bfc5b93
GWIAZDA <US70US@ad.ing.net>
2017-10-04 14:33:13 +0200

    Heroes tutorial

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -4,18 +4,42 @@ import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent } from './app.component';
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
+import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard.component";
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
---
+++ frontend/src/main/frontend/src/app/dashboard.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'my-dashboard',
+  template: '<h3>My DASHBOARD</h3>'
+})
+export class DashboardComponent{
+
+}
